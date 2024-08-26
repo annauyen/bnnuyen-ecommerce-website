406264d68cdb4d84227edf6e9722f88a547bd7d0
Uyen Bui Nguyen Nhi <uyen.buinguyennhi@axonactive.com>
2024-08-26 13:06:36 +0700

    Init project

---
+++ 02-backend/springboot-ecommerce/src/main/java/com/bnnuyen/springboot_ecommerce/entity/Product.java
@@ -0,0 +1,8 @@
+package com.bnnuyen.springboot_ecommerce.entity;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+    
+}
---
+++ 02-backend/springboot-ecommerce/src/test/java/com/bnnuyen/springboot_ecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnnuyen.springboot_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
